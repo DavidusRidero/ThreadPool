5896dc90546426d64ea60182a903d072e59f6ed7
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-04-18 19:33:43 +0530

    Test commit

--- TextBook.cpp
+++ TextBook.cpp
@@ -4,8 +4,14 @@ void hello() {
     std::cout << "Hello World.\n";
 }
 
+void c_hello() {
+    std::cout << "Hello Concurrent World.\n";
+}
+
 int main() {
-    hello();
+    // hello();
+    std::thread thr(c_hello);
+    thr.join();
 
     return 1;
 }
